04baf27493fcdc4bb18e2f138e080c183f7d52ba
Joao Bordignon <joaobordignon@gmail.com>
2020-12-18 20:05:11 -0500

    FrontEnd

---
+++ Project.Presentation/Controllers/EstoqueController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Project.Presentation.Controllers
+{
+    public class EstoqueController : Controller
+    {
+        // GET: Estoque
+        public ActionResult Cadastro()
+        {
+            return View();
+        }
+
+        public ActionResult Consulta()
+        {
+            return View();
+        }
+    }
+}
